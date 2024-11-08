a4256c86f794e8321a52a5688cc9f1774dd4a7c6
Bishara Babish <62569803+bisharababish@users.noreply.github.com>
2024-11-08 22:38:36 +0200

    Add files via upload

---
+++ submit_feedback.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$database = "feedbackdatabase";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $stmt = $conn->prepare("INSERT INTO feedbackdb (name, email, feedback_message) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $name, $email, $feedback_message);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback_message = $_POST['message'];
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    header("Location: index.html");
+    exit();
+}
+?>
